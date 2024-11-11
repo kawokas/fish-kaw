fae2233125481f0739daeeb394c5487bb78950cf
kawokas <usenet.kawokas.jp@gmail.com>
2024-11-11 10:56:19 +0900

    migrate fish 3.3

--- functions/_kaw__git_is_empty.fish
+++ functions/_kaw__git_is_empty.fish
@@ -1,5 +1,5 @@
 function _kaw__git_is_empty -d "Test if a repository is empty"
-    if command git rev-list -n 1 --all > /dev/null ^ /dev/null
+    if command git rev-list -n 1 --all > /dev/null 2> /dev/null
         return 1
     end
 
--- functions/_kaw__git_is_touched.fish
+++ functions/_kaw__git_is_touched.fish
@@ -3,7 +3,7 @@ function _kaw__git_is_touched -d "Test if there are any changes in the working t
         return 1
     end
 
-    command git status --porcelain ^ /dev/null | command awk '
+    command git status --porcelain 2> /dev/null | command awk '
         // {
             z++
             exit 0
